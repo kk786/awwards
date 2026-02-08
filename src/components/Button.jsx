a310941f7978ba9736c9dce0204fc755feaf02b0
kk786 <techkhalid19@gmail.com>
2026-02-08 15:55:43 +0530

    added animations

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Button = ({title, id, rightIcon, leftIcon, containerClass}) => {
+    return (
+        <button id={id} className={`'group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black hover:animate-pulse ${containerClass}'`}>
+            {leftIcon}
+            <span className='relative inline-flex overflow-hidden font-general text-small uppercase'>
+                <div>{title}</div>
+            </span>
+            {rightIcon}
+        </button>
+    );
+};
+
+export default Button;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,8 @@
 import React, {useRef, useState} from 'react';
+import Button from "./Button.jsx";
+import {TiLocationArrow} from "react-icons/ti";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
 
@@ -7,20 +11,44 @@ const Hero = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
-    const totalVideos = 4;
+    const totalVideos = 3;
     const nextVideoRef = useRef(null);
 
     const handleVideoLoad = () => {
         setLoadedVideos((prev) => prev + 1);
     }
 
+    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
     const handleMiniVdClick = () => {
         setHasClicked(true);
 
-        setCurrentIndex((prevIndex) => prevIndex + 1);
+        setCurrentIndex(upcomingVideoIndex);
     }
 
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+    useGSAP(() => {
+        if(hasClicked) {
+            gsap.set('#next-video', {visibility: 'visible'});
+            gsap.to('#next-video', {
+                transformOrigin: 'center center',
+                scale: 1,
+                width: '100%',
+                height: '100%',
+                duration: 1,
+                ease: 'power1.inOut',
+                onStart: () => nextVideoRef.current.play(),
+            })
+            gsap.from('#current-video', {
+                transformOrigin: 'center center',
+                scale: 0,
+                duration: 1.5,
+                ease: 'power1.inOut',
+            })
+        }
+    },{dependencies: [currentIndex], revertOnUpdate: true})
+
 
     return (
         <div className='relative h-dvh w-screen overflow-x-hidden'>
@@ -30,7 +58,7 @@ const Hero = () => {
                         <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in-out hover:scale-100 hover:opacity-100'>
                             <video
                                 ref={nextVideoRef}
-                                src={getVideoSrc(currentIndex + 1)}
+                                src={getVideoSrc(upcomingVideoIndex)}
                                 loop
                                 muted
                                 id='current-video'
@@ -40,8 +68,37 @@ const Hero = () => {
                             />
                         </div>
                     </div>
+                    <video
+                    ref={nextVideoRef}
+                    src={getVideoSrc(currentIndex)}
+                    loop
+                    muted
+                    id='next-video'
+                    className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+                    onLoadedData={handleVideoLoad}/>
+
+                    <video
+                    src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+                    autoPlay
+                    loop
+                    muted
+                    className='absolute left-0 top-0 size-full object-cover object-center'
+                    onLoadedData={handleVideoLoad}/>
+                </div>
+                <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>
+                    G<b>A</b>MING
+                </h1>
+                <div className='absolute left-0 top-0 z-40 size-full'>
+                    <div className='mt-24 px-5 sm:px-10'>
+                        <h1 className='special-font hero-heading text-blue-100'>Redefi<b>n</b>e</h1>
+                        <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>ENTER THE METAGAME LAYER<br/>UNLEASH THE PLAY ECONOMY</p>
+                        <Button id='watch-trailer' title='Watch Trailer' leftIcon={<TiLocationArrow/>} containerClass='bg-yellow-300 flex-center gap-1' />
+                    </div>
                 </div>
             </div>
+            <h1 className='special-font hero-heading absolute bottom-5 right-5 text-black'>
+                G<b>a</b>ming
+            </h1>
         </div>
     );
 };
